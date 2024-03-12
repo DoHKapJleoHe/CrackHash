709f5c030ac5ad0d434b4fd72c327fdfe02959e7
eduardvartazaran <eduardvartazaryan1@gmail.com>
2024-03-12 23:39:08 +0700

    Resolved error with connection to MongoDb replica set: there was dependency collision in manager's pom.xml
    God bless Vasya!

--- ManagerProject/src/main/java/ru/nsu/fit/g20202/vartazaryan/managerproject/ManagerProjectApplication.java
+++ ManagerProject/src/main/java/ru/nsu/fit/g20202/vartazaryan/managerproject/ManagerProjectApplication.java
@@ -2,8 +2,6 @@ package ru.nsu.fit.g20202.vartazaryan.managerproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import ru.nsu.fit.g20202.vartazaryan.managerproject.mongo.TicketsRepository;
 
 @SpringBootApplication
 public class ManagerProjectApplication {
--- ManagerProject/src/main/java/ru/nsu/fit/g20202/vartazaryan/managerproject/config/MongoConfig.java
+++ ManagerProject/src/main/java/ru/nsu/fit/g20202/vartazaryan/managerproject/config/MongoConfig.java
@@ -4,28 +4,33 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import ru.nsu.fit.g20202.vartazaryan.managerproject.mongo.TicketsRepository;
 
-/*@Configuration
-public class MongoConfig
+import java.util.Collection;
+import java.util.Collections;
+
+@Configuration
+@EnableMongoRepositories(basePackages = "ru.nsu.fit.g20202.vartazaryan.managerproject")
+public class MongoConfig extends AbstractMongoClientConfiguration
 {
-    @Value("${spring.data.mongodb.uri}")
-    private String mongoUri;
-
-    @Bean
+    private static final String databaseName = "ticketsdb";
+    @Override
+    protected String getDatabaseName() {
+        return databaseName;
+    }
+    @Override
     public MongoClient mongoClient() {
-        return MongoClients.create(mongoUri);
-    }
+        ConnectionString connectionString = new ConnectionString("mongodb://mongodb1:27017,mongodb2:27018,mongodb3:27019/?replicaSet=rs0");
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
 
-    @Bean
-    public MongoTemplate mongoTemplate() throws Exception {
-        return new MongoTemplate(mongoClient(), "ticketsdb");
+        return MongoClients.create(mongoClientSettings);
     }
-}*/
+    @Override
+    public Collection getMappingBasePackages() {
+        return Collections.singleton("ru.nsu.fit.g20202.vartazaryan.managerproject");
+    }
+}
